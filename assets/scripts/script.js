c07e073a46e6662a9bc02aadd8057848b3ed7c37
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-06-18 12:09:43 -0300

    feat: also almost finished addCart and deleteNum

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,27 +5,33 @@ let elements = {
   description: document.querySelector(".description-game > p"),
   numbers: document.querySelector(".numbers"),
   completeGameBtn: document.querySelector("#complete"),
-  clearGame: document.querySelector("#clear"),
+  clearGameBtn: document.querySelector("#clear"),
+  addToCartBtn: document.querySelector("#cart"),
+  cardCart: document.querySelector(".card-cart"),
+  totalPayment: document.querySelector(".total-value"),
+  gameNumbers: document.querySelector(".game-numbers"),
+  cartContainer: document.querySelector(".cart-container"),
 };
 
 let setIndex = "";
+let cart = [];
+let totalValue = [];
 
 const {
-  lotoBtn,
-  megaBtn,
-  quinaBtn,
   title,
   description,
   numbers,
-  completeGame,
-  clearGame,
+  clearGameBtn,
   completeGameBtn,
   chooseGame,
   game,
+  addToCartBtn,
+  cardCart,
+  totalPayment,
+  gameNumbers,
+  cartContainer,
+  
 } = elements;
-
-
-let cart = [];
 
 fetch("../../services/games.json")
   .then((res) => res.json())
@@ -57,114 +63,135 @@ fetch("../../services/games.json")
           
           num.addEventListener('click', () => {
             if(cart.length < item["max-number"] && cart.indexOf(num.value) == -1){
-              num.setAttribute("class", "clicked");
+              num.setAttribute("clicked", "true");
               num.style.backgroundColor = item.color;
               cart.push(num.value);
               return cart;
             }
           })
           numbers.appendChild(num);
+        
         }
+        setIndex = index;
       });
-
-      setIndex = index;
       
       return setIndex;
     });
   }
   chooseGame();
 });
-
-    // let removeStyle = {
-    //   removeLoto() {
-    //     lotoBtn.classList.remove("active");
-    //     lotoBtn.style.backgroundColor = "transparent";
-    //     lotoBtn.style.color = `${data.types[0].color}`;
-    //   },
-    //   removeMega() {
-    //     megaBtn.classList.remove("active");
-    //     megaBtn.style.backgroundColor = "transparent";
-    //     megaBtn.style.color = `${data.types[1].color}`;
-    //   },
-    //   removeQuina() {
-    //     quinaBtn.classList.remove("active");
-    //     quinaBtn.style.backgroundColor = "transparent";
-    //     quinaBtn.style.color = `${data.types[2].color}`;
-    //   },
-    // };
-
-    // let active = {
-    //   activeLoto() {
-    //     if (lotoBtn.className !== "active") {
-    //       lotoBtn.classList.add("active");
-    //       lotoBtn.style.backgroundColor = `${data.types[0].color}`;
-    //       lotoBtn.style.color = "#FFF";
-
-    //       removeStyle.removeMega();
-    //       removeStyle.removeQuina();
-    //     }
-    //   },
-    //   activeMega() {
-    //     if (megaBtn.className !== "active") {
-    //       megaBtn.classList.add("active");
-    //       megaBtn.style.backgroundColor = `${data.types[1].color}`;
-    //       megaBtn.style.color = "#FFF";
-
-    //       removeStyle.removeLoto();
-    //       removeStyle.removeQuina();
-    //     }
-    //   },
-    //   activeQuina() {
-    //     if (quinaBtn.className !== "active") {
-    //       quinaBtn.classList.add("active");
-    //       quinaBtn.style.backgroundColor = `${data.types[2].color}`;
-    //       quinaBtn.style.color = "#FFF";
-
-    //       removeStyle.removeLoto();
-    //       removeStyle.removeMega();
-    //     }
-    //   },
-    // };
     
     let features = {
       clearGame() {
+        cart = [];
+
         const allNumbers = document.querySelectorAll('#num');
 
         allNumbers.forEach(item => {
-          if(item.getAttribute("checked")) item.removeAttribute("class");
+          if(item.hasAttribute("clicked")) {
+            item.removeAttribute("clicked")
+          };
 
           item.style.backgroundColor = "#ADC0C4";
         })
       },
       completeGame() {
         fetch("../../services/games.json").then(res => res.json()).then(data => {
-          const allNumbers = document.querySelectorAll('#num');
-          
-          while(cart.length < data.types[setIndex]["max-number"]) {
-            let match = Math.ceil(Math.random() * (data.types[setIndex].range - 0) + 1);
-            
-            allNumbers.forEach(num => {
-              if(match == num.value && !num.getAttribute("class")) {
-                num.setAttribute("class", "checked");
-                num.style.backgroundColor = data.types[setIndex].color;
-                cart.push(num.value);
-                return cart;
+          completeGameBtn.addEventListener('click', () => {
+            const allNumbers = document.querySelectorAll('#num');
+              
+              while(cart.length < data.types[setIndex]["max-number"]) {
+                let match = Math.ceil(Math.random() * (data.types[setIndex].range) + 1);
+                
+                allNumbers.forEach(num => {
+                  if(match == num.value && !num.hasAttribute("clicked")) {
+                    num.setAttribute("clicked", "true");
+                    num.style.backgroundColor = data.types[setIndex].color;
+                    cart.push(num.value);
+                    return cart;
+                  }
+                })
               }
-            })
-          }
+          })
         
         });
         return cart;
+      },
+      addToCart() {
+        fetch("../../services/games.json").then(res => res.json().then(data => {
+          if(cart.length == data.types[setIndex]["max-number"]) {
+            gameNumbers.setAttribute(`gamePrice`, data.types[setIndex].price);
+            
+            totalValue.push(Number(gameNumbers.getAttribute("gamePrice")));
+
+            gameNumbers.innerHTML = `
+              <span onClick="this.deleteNumberInCart">
+                <img src="./assets/trash.png" alt="Excluir jogo">
+              </span>
+              <span id="pipe"></span>
+              <div class="numbers-choosed">
+                <p class="cart-numbers">
+                  ${cart}
+                </p>
+                <div class="game-price">
+                  <p style="color:${data.types[setIndex].color};">${data.types[setIndex].type}</p>
+                  <p id="game-price">
+                    R${data.types[setIndex].price.toFixed(2).replace('.', ',')}
+                  </p>
+                </div>
+              </div>
+            `
+
+            cartContainer.appendChild(gameNumbers);
+            
+            this.value();
+           
+            let numberContainer = document.querySelectorAll(".game-numbers");
+
+            numberContainer.forEach(item => {
+              item.addEventListener("click", () => {
+                item.setAttribute("checked", "true");
+                return console.log(item);
+              })
+            })
+          }
+        }))
+      },
+      deleteNumberInCart() {
+        let numberContainer = document.querySelectorAll(".game-numbers");
+
+        numberContainer.forEach(item => {
+          item.addEventListener("click", () => {
+            if(item.hasAttribute("checked")) {
+              item.parentNode.removeChild(item);
+              console.log(item.parentNode)
+              totalValue.splice(totalValue.indexOf(parseFloat(item.getAttribute('gamePrice'))), 1)
+            }
+          })
+        })
+      },
+      value() {
+        const result = totalValue.reduce((acc, item) => {
+          return acc + item;
+        })
+
+        totalPayment.textContent = result.toFixed(2).replace(".", ",");
+
+        if(totalValue.length == 0) totalValue.textContent = "0,00";
+
       }
+      
     };
 
     function clickEvents() {
       completeGameBtn.addEventListener("click", () => {
           features.completeGame();
       }),
-      clearGame.addEventListener("click", () => {
+      clearGameBtn.addEventListener("click", () => {
         features.clearGame();
       });
+      addToCartBtn.addEventListener("click", () => {
+        features.addToCart();
+      })
     }
-
-    clickEvents();
+    clickEvents()
